536727a6846872a40c3b849637caa3fbf0dcdc32
John Goode <johnhgoode@gmail.com>
2015-03-08 19:26:31 -0400

    fixed order of req and res

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var http = require('http');
 
-var server = http.createServer(function(res,req) {
+var server = http.createServer(function(req,res) {
   res.writeHead(200, {'Content-Type':'text/plain'});
   res.end('this is updated johnhgoode.com');
 });
